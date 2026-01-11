694a0da1c47923112a5343332012ac6f611829ad
Zeeshan-Zahoor <shanmohd3939@gmail.com>
2026-01-11 21:50:50 +0530

    UI bug fix

--- Sumio/script.js
+++ Sumio/script.js
@@ -29,11 +29,11 @@ const cancelSubDetailName = document.getElementById('cancel-sub-detail-name-btn'
 
 
 window.addEventListener("offline", () => {
-  showToast("You’re offline. Changes will still be saved.", "info");
+    showToast("You’re offline. Changes will still be saved.", "info");
 });
 
 window.addEventListener("online", () => {
-  showToast("Back online", "success");
+    showToast("Back online", "success");
 });
 
 
@@ -750,6 +750,9 @@ document.querySelector(".detail").addEventListener("click", (e) => {
     }
 });
 
+const deleteSVG = `<div class="clear-detail-btn">
+<svg width="22px" height="28px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M667.8 362.1H304V830c0 28.2 23 51 51.3 51h312.4c28.4 0 51.4-22.8 51.4-51V362.2h-51.3z" fill="#0000"></path><path d="M750.3 295.2c0-8.9-7.6-16.1-17-16.1H289.9c-9.4 0-17 7.2-17 16.1v50.9c0 8.9 7.6 16.1 17 16.1h443.4c9.4 0 17-7.2 17-16.1v-50.9z" fill="#0000"></path><path d="M733.3 258.3H626.6V196c0-11.5-9.3-20.8-20.8-20.8H419.1c-11.5 0-20.8 9.3-20.8 20.8v62.3H289.9c-20.8 0-37.7 16.5-37.7 36.8V346c0 18.1 13.5 33.1 31.1 36.2V830c0 39.6 32.3 71.8 72.1 71.8h312.4c39.8 0 72.1-32.2 72.1-71.8V382.2c17.7-3.1 31.1-18.1 31.1-36.2v-50.9c0.1-20.2-16.9-36.8-37.7-36.8z m-293.5-41.5h145.3v41.5H439.8v-41.5z m-146.2 83.1H729.5v41.5H293.6v-41.5z m404.8 530.2c0 16.7-13.7 30.3-30.6 30.3H355.4c-16.9 0-30.6-13.6-30.6-30.3V382.9h373.6v447.2z" fill="#0000211F1E"></path><path d="M511.6 798.9c11.5 0 20.8-9.3 20.8-20.8V466.8c0-11.5-9.3-20.8-20.8-20.8s-20.8 9.3-20.8 20.8v311.4c0 11.4 9.3 20.7 20.8 20.7zM407.8 798.9c11.5 0 20.8-9.3 20.8-20.8V466.8c0-11.5-9.3-20.8-20.8-20.8s-20.8 9.3-20.8 20.8v311.4c0.1 11.4 9.4 20.7 20.8 20.7zM615.4 799.6c11.5 0 20.8-9.3 20.8-20.8V467.4c0-11.5-9.3-20.8-20.8-20.8s-20.8 9.3-20.8 20.8v311.4c0 11.5 9.3 20.8 20.8 20.8z" fill="#0000211F1E"></path></g></svg>
+</div>`
 
 function addExpenseEntry() {
     if (
@@ -795,8 +798,10 @@ function addExpenseEntry() {
 
         listContainer.insertAdjacentHTML(
             "beforeend", `<div class="individual-detail" data-spent-index = "${(subDetail.subDetailList.length) - 1}">
-                        <span class="spend-detail-span">- ₹${spendingAmount.toLocaleString('en-IN')} (${remarks})</span>
-                        <img src="delete.svg" class="clear-detail-btn" width="20px" alt="Clear">
+                        <div class="individual-detail-inner">
+                            <span class="spend-detail-span">- ₹${spendingAmount.toLocaleString('en-IN')} (${remarks})</span>
+                        </div>
+                        ${deleteSVG}
                     </div>
                     <div class="line"></div>`
         );
@@ -896,7 +901,7 @@ function addCreditAmount() {
 
 // delete credit detail amount
 document.querySelector(".detail").addEventListener("click", (e) => {
-    if(e.target.closest(".clear-credit-detail-btn")) {
+    if (e.target.closest(".clear-credit-detail-btn")) {
 
         const subDetailIndex = Number(e.target.parentElement.parentElement.parentElement.getAttribute('data-sub-index'))
         const currSubListIndex = Number(e.target.parentElement.parentElement.getAttribute('data-spent-index'))
@@ -917,8 +922,8 @@ document.querySelector(".detail").addEventListener("click", (e) => {
         const allocated = Number(remainingProgressionRing.getAttribute('data-total'));
         const remaining = Number(remainingProgressionRing.getAttribute('data-spent'));
 
-        allocatedProgressRing.setAttribute('data-total', `${allocated-deletableAmount}`);
-        allocatedProgressRing.setAttribute('data-spent', `${allocated-deletableAmount}`);
+        allocatedProgressRing.setAttribute('data-total', `${allocated - deletableAmount}`);
+        allocatedProgressRing.setAttribute('data-spent', `${allocated - deletableAmount}`);
 
         remainingProgressionRing.setAttribute('data-total', `${allocated - deletableAmount}`)
         remainingProgressionRing.setAttribute('data-spent', `${remaining - deletableAmount}`)
@@ -1121,8 +1126,9 @@ const createSubExpense = (subDetailObject, subDetailIndex, container) => {
         if (!expenseEntry.iscreditAmount) {
             listContainer.insertAdjacentHTML(
                 "beforeend", `<div class="individual-detail" data-spent-index = "${idx++}">
-                <span class="spend-detail-span">- ₹${(expenseEntry.amount).toLocaleString('en-IN')} (${expenseEntry.remark})</span>
-                <img src="delete.svg" class="clear-detail-btn" width="20px" alt="Clear">
+                <div class="individual-detail-inner">
+                    <span class="spend-detail-span">- ₹${(expenseEntry.amount).toLocaleString('en-IN')} (${expenseEntry.remark})</span>
+                    ${deleteSVG}
                 </div> 
                 <div class="line"></div>`
             )
@@ -1202,7 +1208,7 @@ cancelSubDetailName.addEventListener("click", () => {
 
 // Clear individual expense (event delegation)
 document.querySelector(".detail").addEventListener("click", (e) => {
-    if (e.target.classList.contains("clear-detail-btn")) {
+    if (e.target.closest(".clear-detail-btn")) {
 
         const subDetailIndex = Number(e.target.parentElement.parentElement.parentElement.getAttribute('data-sub-index'))
         const currSubListIndex = Number(e.target.parentElement.getAttribute('data-spent-index'))
